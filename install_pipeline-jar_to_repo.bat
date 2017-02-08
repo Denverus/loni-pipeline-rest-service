55a7fe7e056f4c8ec666af2e5a1a7b18c98669c1
Denis Trotsky <denveru@gmail.com>
2017-02-07 21:01:56 -0800

    Added script to install Pipeline.jar to repo

---
+++ install_pipeline-jar_to_repo.bat
@@ -0,0 +1,6 @@
+mvn deploy:deploy-file -Durl=file://C:\dev\projects\loni-pipeline-rest-service\repo\ -Dfile=lib/Pipeline.jar -DgroupId=usc.edu.loni.pipeline -DartifactId=loni-pipeline -Dpackaging=jar -Dversion=6.4.0
+
+
+
+
+
